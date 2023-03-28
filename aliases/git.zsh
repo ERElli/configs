5cc88f659fea7400cb79875425f988702104333d
ERElli <EricRoy.Elli@gmail.com>
2023-03-27 23:59:15 -0230

    fix indentation

--- aliases/git.zsh
+++ aliases/git.zsh
@@ -1,11 +1,11 @@
 function git_current_branch() {
 	local ref
 	ref=$(git symbolic-ref --quiet HEAD 2> /dev/null)
-  	local ret=$?
-  	if [[ $ret != 0 ]]; then
-			[[ $ret == 128 ]] && return  # no git repo.
+	local ret=$?
+	if [[ $ret != 0 ]]; then
+		[[ $ret == 128 ]] && return  # no git repo.
 		#	ref=$(git rev-parse --short HEAD 2> /dev/null) || return
-		fi
+	fi
 	echo ${ref#refs/heads/}
 }
 
