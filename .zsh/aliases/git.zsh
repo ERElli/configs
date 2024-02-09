7b7bbbbb704bc2de3bc7831cd9f72f7eaa9b47b9
Eric Elli <ericelli@bluedrop.com>
2024-02-09 09:02:18 -0330

    Update git aliases and functions

--- .zsh/aliases/git.zsh
+++ .zsh/aliases/git.zsh
@@ -1,41 +1,50 @@
 function git_current_branch() {
 	local ref
 	ref=$(git symbolic-ref --quiet HEAD 2> /dev/null)
-	local ret=$?
-	if [[ $ret != 0 ]]; then
-		[[ $ret == 128 ]] && return  # no git repo.
+  	local ret=$?
+  	if [[ $ret != 0 ]]; then
+			[[ $ret == 128 ]] && return  # no git repo.
 		#	ref=$(git rev-parse --short HEAD 2> /dev/null) || return
-	fi
+		fi
 	echo ${ref#refs/heads/}
 }
 
 function grp() {
-	cur_dir=$(pwd)
-	dest_dir=$(z -e $1)
-	branch_name=$2
-	echo "Navigating to ${dest_dir}..."	
+	starting_dir=$(pwd)
+	branch_name=${2:-origin/master}
 	z $1
-	echo "Fetching latest refs..."
+	echo "Fetching latest refs from $(z -e $1)..."
 	git fetch
 	hash=$(git rev-parse $branch_name | tr -d '\n')
-	echo "Back to $cur_dir... \n"
-	cd $cur_dir
+
+	cd $starting_dir
+
+	echo "commit-hash $branch_name HEAD: $hash"
 	if [[ $3 = 'cb' ]]; then
 		echo $hash | tr -d '\n' | pbcopy
-		echo "Copying to clipboard..."
+		echo "Commit hash copied to clipboard!"
 	fi
-	echo "commit-hash $2 HEAD: $hash"
+}
+
+function zgo() {
+	starting_dir="$(pwd)"
+	z $1
+	git-open $2 $3
+	cd $starting_dir
 }
 
 alias ga='git add'
 alias gan='git add -N'
 alias gapa='git add --patch'
+alias gbe='git branch | grep "eric/"'
+alias gby='git branch | yank'
 alias gc='git commit --verbose'
 alias gc!='git commit --verbose --amend'
 alias gcmsg='git commit --message'
 alias gcob='git checkout -b'
 alias gco='git checkout'
 alias gd='git diff'
+alias gdc='git diff --compact-summary'
 alias gdno='git diff --name-only'
 alias gds='git diff --staged'
 alias gf='git fetch'
@@ -54,3 +63,56 @@ alias gst='git status'
 alias gsts='git status -s'
 alias gsta='git stash'
 alias gstap='git stash push -u -m'
+
+# Github cli aliases
+alias ginv="gh search prs \
+	--involves @me \
+	--state open \
+	--order desc \
+	--sort created \
+	--json url,number,author,repository,title,updatedAt \
+	--template '
+{{color \"magenta+bu\" \"PULL REQUESTS IM INVOLVED IN\"}}
+
+{{range .}}{{color \"78\" (hyperlink .url (printf \"#%v\" .number))}}{{\"\t\"}}{{truncate 15 .author.login}}{{\"\t\"}}{{color \"blue\" (truncate 30 .repository.name)}}{{\"\t\t\"}}{{truncate 50 .title}}{{\"\n\"}}{{end}}'
+"
+
+alias gpr="gh search prs \
+	--author @me \
+	--state open \
+	--order desc \
+	--sort updated \
+	--json url,number,repository,title,updatedAt \
+	--template '
+{{color \"magenta+bu\" \"MY PULL REQUESTS\"}}
+{{range .}}{{color \"78\" (hyperlink .url (printf \"#%v\" .number))}}{{\"\t\"}}{{color \"blue\" .repository.name}}{{\"\t\t\"}}{{.title}}{{\"\n\"}}{{end}}'
+"
+
+alias grr="gh search prs \
+	--review-requested @me \
+	--state open \
+	--order desc \
+	--sort created \
+	--json url,number,author,repository,title,updatedAt \
+	--template '
+{{color \"magenta+bu\" \"TODO REVIEW REQUESTS\"}}{{\"\n\"}} \
+{{range .}}{{color \"78\" (hyperlink .url (printf \"#%v\" .number))}}{{\"\t\"}}{{truncate 15 .author.login}}{{\"\t\"}}{{color \"blue\" (truncate 30 .repository.name)}}{{\"\t\t\"}}{{truncate 50 .title}}{{\"\n\"}}{{end}}'
+"
+alias grr1="gh search prs \
+	--review-requested @me \
+	--state open \
+	--order asc \
+	--sort created \
+	--json url,number,author,repository,title,updatedAt \
+	--template '\
+	{{- tablerow \"LINK\" \"AUTHOR\" \"REPO\" \"TITLE\" \"UPDATED\" -}}{{tablerender}} \
+	{{- range . -}} \
+	{{- tablerow \
+		(hyperlink .url (printf \"#%v\" .number)) \
+		(truncate 20 .author.login)
+		(truncate 30 .repository.name) \
+		(truncate 30 .title) \
+		(timeago .updatedAt) \
+	-}}
+	{{- end}}' \
+"
