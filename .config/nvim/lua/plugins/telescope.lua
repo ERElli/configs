7f069e2881443ddcbb65b6c7e376f7cb7b944994
ERElli <EricRoy.Elli@gmail.com>
2024-02-13 21:52:51 -0330

    nvim: add gitsigns plugin

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,9 +1,11 @@
 return {
-	"nvim-telescope/telescope.nvim", tag = "0.1.5",
+	"nvim-telescope/telescope.nvim",
+	tag = "0.1.5",
 	lazy = true,
 	dependencies = { "nvim-lua/plenary.nvim" },
 	keys = {
-		{"<leader><Space>", "<cmd>Telescope find_files<cr>", desc = "Find Files"},
+		{ "<leader><Space>", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
+		{ "<leader>/", "<cmd>Telescope live_grep<cr>", desc = "Live Grep (root)" },
 	},
 	config = true,
 }
