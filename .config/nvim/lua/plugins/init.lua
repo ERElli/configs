e5e034aad326ef39cc993942814233690ea1f541
Eric Elli <ericelli@bluedrop.com>
2024-03-12 16:54:53 -0230

    nvim: surround and update

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,3 +1,11 @@
 return {
 	"nvim-lua/plenary.nvim",
+	{
+		"kylechui/nvim-surround",
+		version = "*",
+		event = "VeryLazy",
+		config = function()
+			require("nvim-surround").setup()
+		end,
+	},
 }
