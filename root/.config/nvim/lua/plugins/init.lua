6b4cdc34e07c59e768bb94317c96742f8e458f9b
ERElli <EricRoy.Elli@gmail.com>
2024-05-14 00:58:47 -0230

    nvim: move Comment.nvim to common file

--- root/.config/nvim/lua/plugins/init.lua
+++ root/.config/nvim/lua/plugins/init.lua
@@ -8,4 +8,18 @@ return {
 			require("nvim-surround").setup()
 		end,
 	},
+	{
+		"numToStr/Comment.nvim",
+		event = "VeryLazy",
+		opts = {
+			toggler = {
+				line = "<leader>cl",
+				block = "<leader>cb",
+			},
+			opleader = {
+				line = "<leader>cl",
+				block = "<leader>cb",
+			},
+		},
+	},
 }
