40f74c5edd32959f9258f7b200144b670e6d6e53
Eric Elli <ericelli@bluedrop.com>
2024-11-06 15:48:37 -0330

    nvim: add autocmd for sql commentstring

--- root/.config/nvim/lua/config/autocmds.lua
+++ root/.config/nvim/lua/config/autocmds.lua
@@ -8,3 +8,12 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     vim.highlight.on_yank()
   end,
 })
+
+vim.api.nvim_create_autocmd('FileType', {
+	pattern = 'sql',
+	desc = 'Detect sql files and set the commentstring',
+	group = vim.api.nvim_create_augroup('sql-comment-string', {clear = true}),
+	callback = function(ev)
+		vim.bo[ev.buf].commentstring = '-- %s'
+	end,
+})
