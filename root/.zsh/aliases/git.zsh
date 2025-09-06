f8f321267b4bba26ed1c879032b0f0cc9508af0e
Eric Elli <ericelli@bluedrop.com>
2025-09-06 16:02:06 -0230

    zsh: updated git aliases

--- root/.zsh/aliases/git.zsh
+++ root/.zsh/aliases/git.zsh
@@ -1,29 +1,30 @@
 function git_current_branch() {
-	local ref
-	ref=$(git symbolic-ref --quiet HEAD 2> /dev/null)
-  	local ret=$?
-  	if [[ $ret != 0 ]]; then
-			[[ $ret == 128 ]] && return  # no git repo.
-		#	ref=$(git rev-parse --short HEAD 2> /dev/null) || return
-		fi
-	echo ${ref#refs/heads/}
+    local ref
+    ref=$(git symbolic-ref --quiet HEAD 2> /dev/null)
+    local ret=$?
+    if [[ $ret != 0 ]]; then
+        [[ $ret == 128 ]] && return  # no git repo.
+        #	ref=$(git rev-parse --short HEAD 2> /dev/null) || return
+    fi
+    echo ${ref#refs/heads/}
 }
 
 function grp() {
-	starting_dir=$(pwd)
-	branch_name=${2:-origin/master}
-	z $1
-	echo "Fetching latest refs from $(z -e $1)..."
-	git fetch
-	hash=$(git rev-parse $branch_name | tr -d '\n')
+    starting_dir=$(pwd)
+    branch_name=${2:-origin/master}
+    z $1
+    echo "Fetching latest refs from $(z -e $1)..."
+    git fetch
+    hash=$(git rev-parse $branch_name | tr -d '\n')
 
-	cd $starting_dir
+    cd $starting_dir
 
-	echo "commit-hash $branch_name HEAD: $hash"
-	if [[ $3 = 'cb' ]]; then
-		echo $hash | tr -d '\n' | pbcopy
-		echo "Commit hash copied to clipboard!"
-	fi
+    echo "commit-hash $branch_name HEAD: $hash"
+    if [[ $3 = 'cb' ]]; then
+        echo $hash | tr -d '\n' | pbcopy
+        echo "Commit hash copied to clipboard!"
+    fi
+    
 }
 
 function zgo() {
@@ -51,7 +52,7 @@ alias gdno='git diff --name-only'
 alias gds='git diff --staged'
 alias gf='git fetch'
 alias gl='git log'
-alias glo='git log --oneline --decorate'
+alias glo='git log --pretty=format:"%C(bold blue)%h%Creset %C(white)%s%Creset %C(magenta)[%an]%Creset %C(yellow)%d%Creset %C(cyan)(%ad UTC)%Creset" --date=format-local:%Y-%m-%dT%H:%M:%SZ'
 alias glocb='git log --oneline -n 1 --pretty=format:%s | tr -d "\n" | pbcopy'
 alias gls='git log --stat'
 alias glsp='git log --stat --patch'
@@ -65,8 +66,7 @@ alias gst='git status'
 alias gsts='git status -s'
 alias gsta='git stash'
 alias gstap='git stash push -u -m'
-alias gwl='git worktree list'
-alias gwly='git worktree list | yank'
+alias gwt='git worktree'
 
 # Github cli aliases
 alias ghps='gh pr status'
@@ -104,6 +104,7 @@ alias gpr="gh search prs \
 {{color \"magenta+bu\" \"MY PULL REQUESTS\"}}
 {{range .}}{{color \"78\" (hyperlink .url (printf \"#%v\" .number))}}{{\"\t\"}}{{color \"blue\" .repository.name}}{{\"\t\t\"}}{{.title}}{{\"\n\"}}{{end}}'
 "
+
 alias grr="gh search prs \
 	--review-requested @me \
 	--state open \
