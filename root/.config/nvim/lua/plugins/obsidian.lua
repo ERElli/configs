a64a0e11aa870cbd5fc09470d4c1dd88df45c899
Eric Elli <ericelli@bluedrop.com>
2026-04-16 11:07:52 -0230

    nvim: register obsidian commands

--- root/.config/nvim/lua/plugins/obsidian.lua
+++ root/.config/nvim/lua/plugins/obsidian.lua
@@ -3,6 +3,13 @@ return {
 		"epwalsh/obsidian.nvim",
 		version = "*",  -- recommended, use latest release instead of latest commit
 		ft = "markdown",
+		cmd = {
+			"ObsidianNew", "ObsidianSearch", "ObsidianQuickSwitch", "ObsidianTemplate",
+			"ObsidianTags", "ObsidianBacklinks", "ObsidianFollowLink", "ObsidianLinks",
+			"ObsidianRename", "ObsidianOpen", "ObsidianToday", "ObsidianYesterday",
+			"ObsidianTomorrow", "ObsidianDailies", "ObsidianExtractNote", "ObsidianPasteImg",
+			"ObsidianWorkspace", "ObsidianCheck",
+		},
 		-- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
 		-- event = {
 		-- 	-- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
